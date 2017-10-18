0672fbd583622081806f1b8d7bd866eca9aec71f
Les Aker <me@lesaker.org>
2017-10-17 21:06:35 -0400

    use config yaml file

---
+++ config.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	bucketKey = "S3_BUCKET"
+	pathKey   = "S3_KEY"
+)
+
+type target struct {
+	URL    string
+	Method string
+}
+
+type targetList map[string]target
+
+func (t target) hit() error {
+	m := t.Method
+	if m == "" {
+		m = "GET"
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest(m, t.URL, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(req)
+	return err
+}
+
+func loadConfig() (targetList, error) {
+	var tl targetList
+
+	bucket, path, err := parseEnv()
+	if err != nil {
+		return tl, err
+	}
+
+	client := s3Client()
+
+	obj, err := client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(path),
+	})
+	if err != nil {
+		return tl, err
+	}
+	body, err := ioutil.ReadAll(obj.Body)
+	if err != nil {
+		return tl, err
+	}
+
+	err = yaml.Unmarshal(body, &tl)
+	return tl, err
+}
+
+func parseEnv() (string, string, error) {
+	bucket := os.Getenv(bucketKey)
+	if bucket == "" {
+		return "", "", fmt.Errorf("No bucket provided")
+	}
+	path := os.Getenv(pathKey)
+	if path == "" {
+		return "", "", fmt.Errorf("No path given")
+	}
+	return bucket, path, nil
+}
+
+func s3Client() *s3.S3 {
+	awsConfig := aws.NewConfig().WithCredentialsChainVerboseErrors(true)
+	s := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:            *awsConfig,
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	return s3.New(s)
+}
--- handler.go
+++ handler.go
@@ -2,35 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
 )
 
 const (
-	bucketKey  = "S3_BUCKET"
-	pathKey    = "S3_KEY"
 	failMsg    = "Failed to hit URL %d"
 	successMsg = "Success! Hit %d URLs"
 )
 
-var urls []string
+var tl targetList
 
 // Handle responds to lambda invocations by hitting all known URLs
 func Handle(_ interface{}, _ *runtime.Context) (string, error) {
-	for index, url := range urls {
-		err := hitURL(url)
+	for name, target := range tl {
+		err := target.hit()
 		if err != nil {
-			return logAndExit(failMsg, index, err)
+			return logAndExit(failMsg, name, err)
 		}
 	}
-	return logAndExit(successMsg, len(urls), nil)
+	return logAndExit(successMsg, len(tl), nil)
 }
 
 func logAndExit(template string, data interface{}, err error) (string, error) {
@@ -42,47 +33,12 @@ func logAndExit(template string, data interface{}, err error) (string, error) {
 	return msg, err
 }
 
-func hitURL(url string) error {
-	_, err := http.Get(url)
-	return err
-}
-
 func init() {
-	bucket, path := parseEnv()
-	client := s3Client()
-	obj, err := client.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(path),
-	})
+	var err error
+	tl, err = loadConfig()
 	if err != nil {
 		panic(err)
 	}
-	urlString, err := ioutil.ReadAll(obj.Body)
-	if err != nil {
-		panic(err)
-	}
-	urls = strings.Split(string(urlString), "|")
-}
-
-func parseEnv() (string, string) {
-	bucket := os.Getenv(bucketKey)
-	if bucket == "" {
-		panic("No bucket provided")
-	}
-	path := os.Getenv(pathKey)
-	if path == "" {
-		panic("No path given")
-	}
-	return bucket, path
-}
-
-func s3Client() *s3.S3 {
-	awsConfig := aws.NewConfig().WithCredentialsChainVerboseErrors(true)
-	s := session.Must(session.NewSessionWithOptions(session.Options{
-		Config:            *awsConfig,
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	return s3.New(s)
 }
 
 func main() {}
